8dc4063d0a0cf12b6a643913104756dc8d4dbedf
Francesco <frandferrari@outlook.com>
2022-07-13 17:56:13 -0300

    Entities created

---
+++ src/main/java/com/frandferrari/course_management_system/entities/Administrator.java
@@ -0,0 +1,8 @@
+package com.frandferrari.course_management_system.entities;
+
+public class Administrator {
+	
+	private Students student;
+	private Instructor instructor;
+
+}
---
+++ src/main/java/com/frandferrari/course_management_system/entities/Students.java
@@ -0,0 +1,10 @@
+package com.frandferrari.course_management_system.entities;
+
+public class Students {
+	
+	private Long id;
+	private String name;
+	private String password;
+	private String course;
+
+}
